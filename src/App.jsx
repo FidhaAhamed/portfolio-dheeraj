8f34455c12028911ad3c4ab3ddd909acefd5301e
Fidha Ahamed <fidhaahamedpp@gmail.com>
2025-06-27 12:11:28 +0530

    pages

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Navbar from './Pages/Navbar'
+import Hero from './Pages/Hero'
+import About from './Pages/About'
+import WorkExp from './Pages/WorkExp'
+import Projects from './Pages/Projects'
+import Footer from './Pages/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <WorkExp />
+      <Projects />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <p className='text-5xl'>Hi</p>
+    </div>
+  )
+}
+
+export default Navbar
